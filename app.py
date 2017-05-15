4736bda19e0cb74eb6763db6ca2475b2ec96c296
talwai <aaditya.21@gmail.com>
2017-05-15 15:46:42 -0400

    add bootstrap to app

--- app.py
+++ app.py
@@ -78,4 +78,19 @@ def best_match(d, b):
     pass
 
 if __name__ == '__main__':
+    db.create_all()
+
+    # create beers
+    db.session.add(Beer("ipa"))
+    db.session.add(Beer("pilsner"))
+    db.session.add(Beer("lager"))
+    db.session.add(Beer("stout"))
+    db.session.commit()
+
+    # create donuts
+    db.session.add(Donut("jelly"))
+    db.session.add(Donut("glazed"))
+    db.session.add(Donut("chocolate"))
+    db.session.add(Donut("bavarian"))
+    db.session.commit()
     app.run(host="0.0.0.0", debug=True)
