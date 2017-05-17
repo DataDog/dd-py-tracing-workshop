e1970cf0166209ab0ac3126a349e3a25e03a4228
Aaditya Talwai <aaditya.21@gmail.com>
2017-05-17 08:26:32 -0400

    wire up taster

---
+++ taster.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+
+import random
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/taste")
+def pair():
+    # tid = request.headers.get("X-Trace-Id")
+    # pid = request.headers.get("X-Parent-Span-Id")
+    beer = request.args.get("beer")
+    donut = request.args.get("donut")
+    
+    # TODO: Insert awesome scoring algo here
+    score = random.randint(1, 10)
+    return jsonify(score=score)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5001)
