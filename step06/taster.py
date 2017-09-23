c6f2e30d2d4a7e8a65768ef3af479922bc513617
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-09-23 14:37:26 -0400

    [demo] step06: optimize your endpoint

---
+++ step06/taster.py
@@ -0,0 +1,38 @@
+import random
+
+# import tracing functions
+from ddtrace import tracer
+from ddtrace.contrib.flask import TraceMiddleware
+
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+# trace the Flask application
+tracer.configure(hostname='agent')
+TraceMiddleware(app, tracer, service="taster")
+
+
+@app.route("/taste")
+def taste():
+    # continue the trace
+    trace_id = request.headers.get("X-Datadog-Trace-Id")
+    parent_id = request.headers.get("X-Datadog-Parent-Span-Id")
+    if trace_id and parent_id:
+        span = tracer.current_span()
+        span.trace_id = int(trace_id)
+        span.parent_id = int(parent_id)
+
+    # send the remaining candidates to our taster and pick the best
+    matches = []
+    beer = request.args.get("beer")
+    candidates = request.args.getlist("donuts")
+
+    for candidate in candidates:
+        score = random.randint(1, 10)
+        matches.append((score, candidate))
+
+    best_match = max(matches)
+
+    return jsonify(candidate=best_match[1], score=best_match[0])
