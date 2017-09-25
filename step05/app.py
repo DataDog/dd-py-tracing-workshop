4a4340edb81ba1d50be5b6f25ae4861234ee8058
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-09-25 09:38:24 -0400

    [demo] step05: instrument taster (#5)

---
+++ step05/app.py
@@ -0,0 +1,114 @@
+import requests
+
+from flask import request, jsonify
+from bootstrap import create_app
+
+from models import Beer, Donut
+from stats import DonutStats
+
+# import tracing functions
+from ddtrace import tracer
+from ddtrace.contrib.flask import TraceMiddleware
+
+
+# initialize Flask application
+app = create_app()
+
+# trace the Flask application
+TraceMiddleware(app, tracer, service="match-maker")
+
+# some simple routes
+@app.route('/ping')
+def ping():
+    """
+    A health check
+    """
+    return "200 OK"
+
+
+@app.route('/beers')
+def beers():
+    """
+    List all beers
+    """
+    # Get beers from the database
+    return jsonify(beers=[b.serialize() for b in Beer.query.all()])
+
+
+@app.route('/donuts')
+def donuts():
+    """
+    List all donuts
+    """
+    return jsonify(donuts=[d.serialize() for d in Donut.query.all()])
+
+
+@app.route('/beers/<name>')
+def beer(name):
+    """
+    Get a beer by name
+    """
+    return jsonify(Beer.query.filter_by(name=name).first().serialize())
+
+
+@app.route('/donuts/<name>')
+def donut(name):
+    """
+    Get a donut by name
+    """
+    return jsonify(Donut.query.filter_by(name=name).first().serialize())
+
+
+@app.route('/pair/beer')
+def pair():
+    """A complex endpoint that makes a request to another Python service"""
+    name = request.args.get('name')
+
+    with tracer.trace("beer.query", service="beer-database"):
+        beer = Beer.query.filter_by(name=name).first()
+
+    # force a query
+    with tracer.trace("donuts.query", service="beer-database"):
+        Donut.query.all()
+
+    with tracer.trace("donuts.query") as span:
+        span.set_tag('beer.name', name)
+        match = best_match(beer)
+    return jsonify(match=match)
+
+
+def best_match(beer):
+    # find our optimal sugar level Donuts above or below this level
+    # will certainly not be a good match
+    db = DonutStats.instance()
+    optimal_sugar_level = db.get_optimal_sugar_level(beer.hops)
+    candidates = db.get_by_sugar_level(optimal_sugar_level, limit=10)
+
+    # send the remaining candidates to our taster and pick the best
+    max_score = -1
+    best_match = None
+
+    for candidate in candidates:
+        try:
+            # propagate the trace context between the two services
+            span = tracer.current_span()
+            headers = {
+                "X-Datadog-Trace-Id": str(span.trace_id),
+                "X-Datadog-Parent-Span-Id": str(span.span_id),
+            }
+
+            resp = requests.get(
+                "http://taster:5001/taste",
+                params={"beer": beer.name, "donut": candidate},
+                timeout=2,
+                headers=headers,
+            )
+        except requests.exceptions.Timeout:
+            continue
+
+        score = resp.json()["score"]
+        if score > max_score:
+            max_score = score
+            best_match = candidate
+
+    return best_match
